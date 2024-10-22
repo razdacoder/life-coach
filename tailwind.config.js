73d544f35db007cb77f4bc5c89d0ac28779d61b3
Ramon Rasheed <ramonrash2@gmail.com>
2024-10-22 14:36:24 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      sans: ["Rajdhani", "sans-serif"],
+      source: ["Source Sans 3", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        primary: "#C3002F",
+        text: "#606060",
+        "black-2": "#222D35",
+      },
+    },
+  },
+  plugins: [],
+};
